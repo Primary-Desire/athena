2b1994791fdfec1288f4f7703c34b9a996bea8ae
LauHonban <hanbin2498@outlook.com>
2021-12-17 21:02:38 +0800

    获取用户列表接口

--- src/main/java/cn/ruiheyun/athena/admin/controller/impl/SysUserControllerImpl.java
+++ src/main/java/cn/ruiheyun/athena/admin/controller/impl/SysUserControllerImpl.java
@@ -1,6 +1,7 @@
 package cn.ruiheyun.athena.admin.controller.impl;
 
 import cn.ruiheyun.athena.admin.controller.ISysUserController;
+import cn.ruiheyun.athena.admin.entity.SysUser;
 import cn.ruiheyun.athena.admin.service.ISysUserService;
 import cn.ruiheyun.athena.common.response.JsonResult;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = {"/api/v1/admin/user"})
@@ -20,6 +22,10 @@ public class SysUserControllerImpl implements ISysUserController {
     @Override
     @RequestMapping(value = {"/list"}, method = {RequestMethod.GET})
     public Object listUser() {
-        return Mono.just(sysUserService.list()).map(userList -> JsonResult.success("查询成功", userList));
+        List<SysUser> userList = sysUserService.lambdaQuery()
+                .select(SysUser::getId, SysUser::getSn, SysUser::getUsername, SysUser::getLastLoginIp, SysUser::getLastLoginRegion, SysUser::getStatus)
+                .list();
+        return Mono.just(userList)
+                .map(list -> JsonResult.success("查询成功", list));
     }
 }
